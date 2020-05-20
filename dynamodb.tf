7275b08e5c6c36aa0debf2cb7f0ca5277eaa0538
Pedro Pessoa <prp@pobox.com>
2020-05-20 10:00:09 +0100

    update terraform version and new syntax with formating

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,7 +1,7 @@
 # Defines the lock table
 
 resource "aws_dynamodb_table" "tfstatelock" {
-  count          = "${length(var.my_inception_projects)}"
+  count          = length(var.my_inception_projects)
   name           = "${var.my_inception_organization}_${var.my_inception_environment}_${var.my_inception_domain}_${var.my_inception_projects[count.index]}_tfstatelock"
   read_capacity  = 1
   write_capacity = 1
@@ -13,7 +13,7 @@ resource "aws_dynamodb_table" "tfstatelock" {
   }
 
   tags {
-    Component = "ops"
+    Component          = "ops"
     ManagedByTerraform = "yes"
   }
 }
