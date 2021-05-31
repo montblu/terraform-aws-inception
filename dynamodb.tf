dc40dd19db4d1569c8dbf64e0d089f1f62b6d1c3
Tiago Carrondo <tcarrondo@angulosolido.pt>
2021-05-31 06:51:17 +0100

    remove counts

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,8 +1,7 @@
 # Defines the lock table
 
 resource "aws_dynamodb_table" "tfstatelock" {
-  count          = length(var.my_inception_projects)
-  name           = "${var.my_inception_organization}_${var.my_inception_environment}_${var.my_inception_domain}_${var.my_inception_projects[count.index]}_tfstatelock"
+  name           = "${var.my_inception_organization}_${var.my_inception_environment}_${var.my_inception_domain}_${var.my_inception_project}_tfstatelock"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
