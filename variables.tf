feb993b0c25f41014d27c3b9a8a495e8a315f6dc
Tiago Carrondo <tcarrondo@angulosolido.pt>
2021-05-31 06:51:31 +0100

    tf13 way

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "my_inception_organization" { type = string }
-variable "my_inception_environment" { type = string }
-variable "my_inception_domain" { type = string }
-variable "my_inception_projects" { type = list }
+variable my_inception_organization { type = string }
+variable my_inception_environment { type = string }
+variable my_inception_domain { type = string }
+variable my_inception_project { type = string }
