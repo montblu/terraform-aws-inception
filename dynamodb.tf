ea34a04e3c0cc124f8bbfc7eeaaf87913b36a26a
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-12-14 17:16:37 +0000

    dynamodb.tf: Use module

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,9 @@
 # Defines the lock table
 
-resource "aws_dynamodb_table" "tfstatelock" {
+module "dynamodb_table" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "1.1.0"
+
   name           = "${var.my_inception_organization}_${var.my_inception_environment}_${var.my_inception_domain}_${var.my_inception_project}_tfstatelock"
   read_capacity  = 1
   write_capacity = 1
