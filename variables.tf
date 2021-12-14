03eea5227c4e8e7955504018d0fdd76a766f271a
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-12-14 17:12:58 +0000

    variables.tf: Add disable force_destroy and disable versioning

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "my_inception_organization" { type = string }
 variable "my_inception_environment" { type = string }
 variable "my_inception_domain" { type = string }
 variable "my_inception_project" { type = string }
+
+variable "my_inception_enable_versioning" {
+  type    = bool
+  default = true
+}
+
+variable "my_inception_disable_force_destroy" {
+  type    = bool
+  default = false
+}
