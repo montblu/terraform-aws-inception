3e7c361c8e953d235740d33b2e1b536f68370493
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-12-27 13:47:57 +0000

    dynamodb.tf: Move to the dynamodb resource to ignore changes

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,9 +1,6 @@
 # Defines the lock table
 
-module "dynamodb_table" {
-  source  = "terraform-aws-modules/dynamodb-table/aws"
-  version = "1.1.0"
-
+resource "aws_dynamodb_table" "tfstatelock" {
   name           = "${var.my_inception_organization}_${var.my_inception_environment}_${var.my_inception_domain}_${var.my_inception_project}_tfstatelock"
   read_capacity  = 1
   write_capacity = 1
