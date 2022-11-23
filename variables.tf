a4f024ff79ebdbaf2c50e1ed49b31a204e77e3ad
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-11-23 19:43:05 +0000

    variables.tf: Add my_inception_tags

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "my_inception_environment" { type = string }
 variable "my_inception_domain" { type = string }
 variable "my_inception_project" { type = string }
 
+variable "my_inception_tags" {
+  type    = map(any)
+  default = {}
+}
+
 variable "my_inception_enable_versioning" {
   type    = bool
   default = true
