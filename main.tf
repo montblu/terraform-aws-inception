c8351121a4e83e6ce701a6292fc3d5581a2b8f0b
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-12-20 16:41:56 +0000

    main.tf: Fix for_each that should be a count

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_dynamodb_table" "main" {
 ################################################################################
 
 resource "local_file" "main" {
-  for_each = var.generate_backend_config_file ? 1 : 0
+  count = var.generate_backend_config_file ? 1 : 0
 
   content = templatefile("${path.module}/utils/templates/backend_config.tpl", {
     profile       = var.profile,
